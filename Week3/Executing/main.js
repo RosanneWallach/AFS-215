8e9963da3d4ed51f9c056153929b61237c7e001d
Rosanne Wallach <Rosanne.wallach@bryanuniversity.edu>
2022-03-03 08:17:12 -0500

    Update 3.1

---
+++ Week3/Executing/main.js
@@ -0,0 +1,14 @@
+const User = require("../user");
+
+let user1 = new User("Rosie", 32);
+let user2 = new User("Dylan", 34);
+
+user1.greetUser();
+user2.greetUser();
+
+
+user1.getStrVal();
+user2.add();
+user2.sub();
+user2.mul();
+user2.div();
